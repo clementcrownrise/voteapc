af4e3521499f04b3eb281870abbd613b1d2723f0
clementcrownrise <clementcrownrise@gmail.com>
2014-02-26 07:15:14 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require 'php-sdk/facebook.php;'; 
+$facebook = new Facebook(array(
+    'appId' =>'603661543043829',
+    'secret' => '27aeca3202fe7ff31ae5a2ecd9314b1c'
+))
+        
+        
+?>
+
+
+<html>
+<head></head>
+<body>
+    
+    <h1>VOte APC</h1>
+</body>
+</html>
